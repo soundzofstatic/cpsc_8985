f3772e83e0c9907b267940bddbdbc80fe4d11eb9
LAPTOP-OA970BC8\13126 <chowdaryanusha.28@gmail.com>
2020-09-21 21:16:38 -0500

    add model,add migration,add controller

---
+++ app/Http/Controllers/PromotedBusinessController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PromotedBusiness;
+use Illuminate\Http\Request;
+
+class PromotedBusinessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PromotedBusiness  $promotedBusiness
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PromotedBusiness $promotedBusiness)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PromotedBusiness  $promotedBusiness
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PromotedBusiness $promotedBusiness)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PromotedBusiness  $promotedBusiness
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PromotedBusiness $promotedBusiness)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PromotedBusiness  $promotedBusiness
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PromotedBusiness $promotedBusiness)
+    {
+        //
+    }
+}
