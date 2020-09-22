70f503b8bb0a2cdc217216c1e6d2763fd6a49c8d
Daniel Paz <d.paz@live.com>
2020-09-21 21:33:05 -0500

    Add controller, model, and migrations

---
+++ database/migrations/2020_09_22_014649_create_alerts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('business_id')->unsigned();
+            $table->boolean('is_viewed');
+            $table->text('text');
+            $table->string('alert_type', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+}
