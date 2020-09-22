a2ca41eb8b3046b8d3c52e31aa9d02cdc25f3009
Mythri <mythri.pittala1997@gmail.com>
2020-09-21 21:18:53 -0500

    add model, add migration, add controller

---
+++ app/Http/Controllers/BusinessCheckInController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BusinessCheckIn;
+use Illuminate\Http\Request;
+
+class BusinessCheckInController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BusinessCheckIn  $businessCheckIn
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BusinessCheckIn $businessCheckIn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BusinessCheckIn  $businessCheckIn
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BusinessCheckIn $businessCheckIn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BusinessCheckIn  $businessCheckIn
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BusinessCheckIn $businessCheckIn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BusinessCheckIn  $businessCheckIn
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BusinessCheckIn $businessCheckIn)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_09_22_001301_create_businesses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusinessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->boolean('is_active');
+            $table->string('name', 15);
+            $table->text('address');
+            $table->text('hours');
+            $table->string('est_date', 15);
+            $table->text('description');
+            $table->integer('dollar_rating');
+            $table->string('web_url', 120);
+            $table->string('menu_url', 120);
+            $table->string('contact_phone', 11);
+            $table->string('contact_email', 64);
+            $table->integer('view_count');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('businesses');
+    }
+}
