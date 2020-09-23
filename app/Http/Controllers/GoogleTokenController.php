246e7ae9e2c690ae5ceb246da2693c05fa9f93b8
Daniel Paz <d.paz@live.com>
2020-09-22 22:08:53 -0500

    Add missing Model and Controller

---
+++ app/Http/Controllers/GoogleTokenController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GoogleToken;
+use Illuminate\Http\Request;
+
+class GoogleTokenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GoogleToken  $googleToken
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GoogleToken $googleToken)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\GoogleToken  $googleToken
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GoogleToken $googleToken)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GoogleToken  $googleToken
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GoogleToken $googleToken)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GoogleToken  $googleToken
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GoogleToken $googleToken)
+    {
+        //
+    }
+}
