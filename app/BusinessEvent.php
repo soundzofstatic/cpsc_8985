8d9a91442a0e4eeb02b42bf7a83f12c8ce238205
Bhavana <brajani@student.govst.edu>
2020-09-28 20:28:16 -0500

    Added relationships for businessservice and services

--- app/BusinessEvent.php
+++ app/BusinessEvent.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class BusinessEvent extends Model
 {
     //
+    public function user() {
+        return $this->belongsTo(
+            User::class,
+            'user_id',
+            'id'
+        );
+    }
+    public function Business()
+    {
+        return $this->hasOne(
+            Business::class,
+            'user_id',
+            'id'
+        );
+    }
 }
--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    // Relationships
+        public function user() {
+            return $this->belongsTo(
+                User::class,
+                'user_id',
+                'id'
+            );
+       }
+        public function business() {
+            return $this->belongsTo(
+                Business::class,
+                'business_id',
+                'id'
+            );
+       }
 }
