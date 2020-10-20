51b066766bf205b89a46eddc5b0ac3da7bb2d7ce
LAPTOP-OA970BC8\13126 <chowdaryanusha.28@gmail.com>
2020-10-19 19:43:25 -0500

    <added bookmark code>

--- resources/views/console/user/business/home.blade.php
+++ resources/views/console/user/business/home.blade.php
@@ -15,6 +15,55 @@
             </div>
             <div class="col-md-12">
                 {{--  todo - Render Data            --}}
+                <div class="d-flex align-items-center">
+
+                    <div class="basr-social-share social">
+                        <ul class="">
+                            <li>
+                                <a class="facebook" href="">
+                                    <i class="fa fa-facebook"></i>
+                                    <span>Facebook</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="twitter" href="">
+                                    <i class="fa fa-twitter"></i>
+                                    <span>Twitter</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="googleplus" href="">
+                                    <i class="fa fa-google-plus"></i>
+                                    <span>Google Plus</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="linkedin" href="">
+                                    <i class="fa fa-linkedin"></i>
+                                    <span>Linkedin</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a class="tumblr" href="">
+                                    <i class="fa fa-tumblr"></i>
+                                    <span>Tumblr</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+
+
+
+
+                </div>
             </div>
         </div>
 {{--        <div class="row mb-5">--}}
