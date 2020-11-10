161bffc877105a1126ef949c1e612e6a66dee306
Daniel Paz <d.paz@live.com>
2020-11-09 20:17:32 -0600

    Fix erroneously removed start_date attribute when creating BusinessEvent

--- app/Http/Controllers/BusinessEventController.php
+++ app/Http/Controllers/BusinessEventController.php
@@ -114,7 +114,7 @@ class BusinessEventController extends Controller
             $BusinessEvent->business_id = $business->id;
             $BusinessEvent->name = $request->input('name');
             $BusinessEvent->description = $request->input('description');
-            $BusinessEvent-> = $start_date;
+            $BusinessEvent->start_date = $start_date;
             $BusinessEvent->save();
 
             return redirect()
