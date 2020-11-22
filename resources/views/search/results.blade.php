b8232b4d172fa7c2bc60398d35f673fcafd2daa0
LAPTOP-OA970BC8\13126 <chowdaryanusha.28@gmail.com>
2020-11-21 19:51:44 -0600

    display of promoted business in search results page

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -6,3 +6,23 @@
     @include ('themes.localsdirectory.layout.section.search.search')
     @include ('themes.localsdirectory.layout.section.search.results')
 @endsection
+@section ('content')
+@if(!empty($promotedBusiness3))
+    <section class="promo-section promo-3 set-bg p-5" data-setbg="{{ parse_url(asset('img/hero-bg.jpg'), PHP_URL_PATH) }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="promo-text">
+                        <h2>{{ $promotedBusiness3->business->name }}</h2>
+                        <p>Some teaser text or business info line</p>
+                        <a href="{{ route('business.home', ['business' => $promotedBusiness3->business->id]) }}" class="btn btn-lg btn-info">See business</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="promo-label">
+            <span>Promotion</span>
+        </div>
+    </section>
+@endif
+@endsection
